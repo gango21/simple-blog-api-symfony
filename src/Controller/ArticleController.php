6de556c3727bfabdf0e2c493ad860cb690183df4
morgan <morgan.tag@gmail.com>
2020-07-08 06:40:15 +0200

    initial commit

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Article;
+use JMS\Serializer\SerializerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/articles/{id}", name="article_show")
+     */
+    public function showAction($id, SerializerInterface $serialize)
+    {
+        $repository = $this->getDoctrine()->getRepository(Article::class); 
+                    $article = $repository->findOneBy([
+                        'id' => $id   
+                    ]);
+        
+        $data = $serialize->serialize($article, 'json');
+
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    /**
+     * @Route("/articles/create", methods={"POST"}, name="article_create")
+     */
+
+     public function createAction(Request $request, SerializerInterface $serialize, EntityManagerInterface $manager)
+     {
+        $data = $request->getContent();
+        $article = $serialize->deserialize($data, Article::class, 'json');
+
+        $manager->persist($article);
+        $manager->flush();
+
+        return new Response('', Response::HTTP_CREATED);
+     }
+
+
+     /**
+     * @Route("/articleslist", methods={"GET"}, name="articles_list")
+     */
+
+    public function listAction(SerializerInterface $serialize)
+    {
+       $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
+       $data = $serialize->serialize($articles, 'json');
+
+       $response = new Response($data);
+       $response->headers->set('Content-Type', 'application/json');
+
+       return $response;
+
+    }
+}
