e9e9135a41b723ec44a0f896cbcde72a388d9139
morgan <morgan.tag@gmail.com>
2020-07-11 07:56:10 +0200

    create Author action

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Author;
 use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -29,6 +31,20 @@ class AuthorController extends AbstractController
         ]);
 
         return $response;
+    }
+
+    /**
+     * @Route("/authors", methods={"POST"}, name="author_create")
+     */
+    public function createAction(Request $request, SerializerInterface $serializer, EntityManagerInterface $manager)
+    {
+        $data = $request->getContent();
+        $author = $serializer->deserialize($data, Author::class, "json");
+
+        $manager->persist($author);
+        $manager->flush();
+
+        return new Response('', Response::HTTP_CREATED);
     }
 
 }
